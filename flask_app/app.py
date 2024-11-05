dfefeb61dee3eb35ab597fa54e508eae659ca4f4
Ericjr-a <ericjr.a17@gmail.com>
2024-11-05 00:24:11 +0000

    update

--- flask_app/app.py
+++ flask_app/app.py
@@ -33,5 +33,5 @@ def lois():
 def kiki(): 
     return render_template('kiki.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
